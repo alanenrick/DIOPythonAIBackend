8a905460c80c17e826ac3e9b175dc52300702d87
Alan Henrique <alanenrick@hotmail.com>
2024-04-12 22:24:19 -0300

    Update ajustes_desafio_01.py

--- Desafio_1/ajustes_desafio_01.py
+++ Desafio_1/ajustes_desafio_01.py
@@ -2,14 +2,6 @@ import time
 from decimal import Decimal
 
 decimal_precision = Decimal('0.01')
-menu = """
-
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-
-=> """
 
 saldo = Decimal('0')
 limite = Decimal('500')
@@ -17,39 +9,75 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+sacar = "[s] Sacar" if saldo > 0 else "[s] Sacar (Não disponível)"
+
+menu = f"""
+
+[d] Depositar
+{sacar}
+[e] Extrato
+[q] Sair
+
+=> """
+
 while True:
+    
     opcao = input(menu)
 
     if opcao == "d":
+        
         valor_deposito = Decimal(input("Informe o valor do depósito: "))
+        
         if valor_deposito > 0:
+            
             saldo += valor_deposito
+            
             extrato += f"{time.strftime('%d/%m/%y %X')} |    Depósito   |   R$ {valor_deposito:.2f}\n"
+        
         else:
             print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "s":
-        valor_saque = Decimal(input("Informe o valor do saque: "))
-        if 0 < valor_saque <= saldo and valor_saque <= limite and numero_saques < LIMITE_SAQUES:
-            saldo -= valor_saque
-            extrato += f"{time.strftime('%d/%m/%y %X')} |    Saque      |   R$ {valor_saque:.2f}\n"
-            numero_saques += 1
+        com_saldo = saldo > 0
+
+        if not com_saldo:
+            print("Saque não disponível")
+        
         else:
-            print("Operação falhou! Verifique seu saldo, limite de saque ou número de saques.")
+            valor_saque = Decimal(input("Informe o valor do saque: "))
+
+            if 0 < valor_saque <= saldo and valor_saque <= limite and numero_saques < LIMITE_SAQUES:
+            
+                saldo -= valor_saque
+            
+                extrato += f"{time.strftime('%d/%m/%y %X')} |    Saque      |   (R$ {valor_saque:.2f})\n"
+            
+                numero_saques += 1
+       
+            else:
+                print("Operação falhou! Verifique seu saldo, limite de saque ou número de saques.")
 
     elif opcao == "e":
-        data_hora = time.strftime('%d/%m/%y %X')
-        extrato_formatado = f"{data_hora} | Operação | Valor\n" if not extrato else extrato
-        saldo_formatado = f"Saldo: R$ {saldo:.2f}"
-        print("\n================ EXTRATO ================")
+
+        # data_hora = time.strftime('%d/%m/%y %X')
+        data_hora = time.localtime()
+        data = time.strftime('%d/%m/%y',data_hora)
+        hora = time.strftime('%H:%M:%S',data_hora)
+
+        extrato_formatado = f"Não há registro de movimentações.\n" if not extrato else extrato
+        
+        saldo_formatado = f"Saldo em {data} {hora}:       R$ {saldo:.2f}"
+
+        print("\n================== EXTRATO ==================")
+        print("\n=============================================")
         print("Data/Hora          |   Operação    |    Valor")
-        print("==========================================")
+        print("=============================================")
         print(extrato_formatado)
         print(f"\n{saldo_formatado}")
-        print("===================FIM===================")
+        print("=====================FIM=====================")
 
     elif opcao == "q":
         break
 
     else:
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Operação inválida, por favor selecione uma opção disponível.")
