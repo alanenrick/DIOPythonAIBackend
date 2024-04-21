bcb07cdcafb185a2facdc1beec924f8ae09ce9b8
Alan Henrique <160517474+alanenrick@users.noreply.github.com>
2024-04-21 06:45:39 -0300

    Create Desafio_de_codigo_01.py

---
+++ Desafio_de_codigo_01/Desafio_de_codigo_01.py
@@ -0,0 +1,11 @@
+# TODO: Crie uma Função: recomendar_plano para receber o consumo médio mensal:
+
+    
+# TODO: Crie uma Estrutura Condicional para verifica o consumo médio mensal 
+# TODO: Retorne o plano de internet adequado:
+    
+
+# Solicita ao usuário que insira o consumo médio mensal de dados:
+consumo = float(input())
+# Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
+print(recomendar_plano(consumo))
